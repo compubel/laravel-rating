71e13ad62255b7b7d9bfba8dae9192f29b44f54f
CasperBE <jasper@compubel.be>
2020-01-12 15:03:40 +0000

    Apply fixes from StyleCI

--- tests/Models/Member.php
+++ tests/Models/Member.php
@@ -2,8 +2,8 @@
 
 namespace Compubel\Rating\Test\Models;
 
-use Compubel\Rating\Rate;
 use Compubel\Rating\Contracts\Rating;
+use Compubel\Rating\Rate;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Member extends Authenticatable implements Rating
